fdfb0e5a15cafdb56f416d7ad6593f7c34096b22
Dakota Thompson <scriptif@gmail.com>
2021-02-12 12:38:33 -0600

    Replace parcel with webpack

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -4,33 +4,38 @@ export class Login extends Component {
   render() {
     return (
       <div class="login">
-        <div className="form">
-          <div className="form__group">
-            <div className="form__label">E-Mail</div>
-            <div className="form__field">
-              <input
-                className="form__input"
-                type="email"
-                placeholder="john.doe@example.com"
-              />
+        <div className="login__brand">
+          <div className="login__logo">BookIt!</div>
+        </div>
+        <div className="login__form">
+          <div className="form">
+            <div className="form__group">
+              <div className="form__label">E-Mail</div>
+              <div className="form__field">
+                <input
+                  className="form__input"
+                  type="email"
+                  placeholder="john.doe@example.com"
+                />
+              </div>
             </div>
-          </div>
 
-          <div className="form__group">
-            <div className="form__label">Password</div>
-            <div className="form__field">
-              <input
-                className="form__input"
-                type="password"
-                placeholder="Password"
-              />
+            <div className="form__group">
+              <div className="form__label">Password</div>
+              <div className="form__field">
+                <input
+                  className="form__input"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
             </div>
-          </div>
 
-          <div className="form__group">
-            <button type="submit" class="form__submit">
-              Submit
-            </button>
+            <div className="form__group">
+              <button type="button" class="form__submit">
+                Submit
+              </button>
+            </div>
           </div>
         </div>
       </div>
---
+++ packages/client/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const distPath = path.resolve("/dist");
+
+module.exports = (env, argv) => {
+  return {
+    mode: "development",
+    devServer: {
+      contentBase: distPath,
+      port: 8080,
+      host: "0.0.0.0",
+      disableHostCheck: true,
+    },
+    entry: [path.resolve("src/index.tsx"), path.resolve("src/scss/index.scss")],
+    output: {
+      path: distPath,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "index.html",
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/i,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+  };
+};
