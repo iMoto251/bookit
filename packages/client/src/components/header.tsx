58cedc60993b150d44f761db4a5c8113564b22d1
William Burkes <dbrider251@gmail.com>
2021-03-12 15:21:05 -0600

    Updates for moving from Preact to React

--- packages/client/src/components/header.tsx
+++ packages/client/src/components/header.tsx
@@ -1,15 +1,18 @@
 import * as React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-const homeLink: string = "/";
+import { Link } from "react-router-dom";
+import * as Logo from "../images/logo.svg";
 
 export default function Header() {
   return (
     <div>
       <nav className="nav">
-        <h1>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login </Link>
-          <Link to="/help">Help </Link>
+        <h1>  
+          <ul className="nav__bar">
+            <a href='/'><img src={Logo} height={50} /></a>
+            <Link to="/" className="nav__links">Home </Link>
+            <Link to="/login" className="nav__links">Login </Link>
+            <Link to="/help" className="nav__links">Help</Link>
+          </ul>
         </h1>
       </nav>
     </div>
--- packages/client/src/containers/home.tsx
+++ packages/client/src/containers/home.tsx
@@ -2,12 +2,10 @@ import * as React from "react";
 
 export default function HomePage() {
   return (
-    <div>
       <div className="login">
         <div className="login__brand">
           <div className="login__logo">Home Page</div>
         </div>
       </div>
-    </div>
   );
 }
