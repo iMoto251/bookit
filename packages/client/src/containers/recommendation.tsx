ce44d09bda7d5be2261ddc1768e0e0b9df1cd58f
William Burkes <dbrider251@gmail.com>
2021-03-25 21:56:11 -0500

    Added recommendation pages

---
+++ packages/client/src/containers/recommendation.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+export default function Recommendation() {
+  return (
+    <div className="rec">
+      <div className="rec__filters">
+        <select name="authors" id="authors">
+          <option value="Dickenson">Dickenson</option>
+          <option value="Poe">Poe</option>
+          <option value="Smith">Smith</option>
+        </select>
+        <select name="price" id="price">
+          <option value="0">Free</option>
+          <option value="20">Under 20</option>
+          <option value="40">Under 40</option>
+        </select>
+        <select name="genre" id="genre">
+          <option value="Horror">Horror</option>
+          <option value="Scifi">Sci-Fi</option>
+          <option value="Nonfiction">Non-Fiction</option>
+        </select>
+      </div>
+      <div>
+        <h1 className="rec__title">Book Title</h1>
+        <p className="rec__desc">Author, Genre, Short Description, ISBN</p>
+      </div>
+      <hr />
+      <div>
+        <h1 className="rec__title">Book Title</h1>
+        <p className="rec__desc">Author, Genre, Short Description, ISBN</p>
+      </div>
+      <hr />
+      <div>
+        <h1 className="rec__title">Book Title</h1>
+        <p className="rec__desc">Author, Genre, Short Description, ISBN</p>
+      </div>
+      <hr />
+      <div>
+        <h1 className="rec__title">Book Title</h1>
+        <p className="rec__desc">Author, Genre, Short Description, ISBN</p>
+      </div>
+      <hr />
+      <div>
+        <h1 className="rec__title">Book Title</h1>
+        <p className="rec__desc">Author, Genre, Short Description, ISBN</p>
+      </div>
+      <hr />
+      <div>
+        <h1 className="rec__title">Book Title</h1>
+        <p className="rec__desc">Author, Genre, Short Description, ISBN</p>
+      </div>
+      
+    </div>
+  );
+}
