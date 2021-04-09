b4b38a152b51226c3f429e65891744832aba21bc
wlkeith0 <whitneylynnkeith@gmail.com>
2021-04-09 16:04:24 -0500

    seeders file fixed

--- packages/server/db/seeders/seed-user.js
+++ packages/server/db/seeders/seed-user.js
@@ -2,12 +2,12 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
         {
-            id: '87cf44fe-cc1f-4fa7-a936-d15dbb122bcc',
+            id: '1',
             email: 'test.admin@gmail.com',
             password: '$2a$10$xYkomxBysjVimxtP7flrTee/iiueeZp5e/FRmu9NKyTpeLpS7O43a'
         },
         {
-            id: '436e49ad-3b04-40d6-b8da-f6d26f45ed17',
+            id: '2',
             email: 'test.member@gmail.com',
             password: '$2a$10$xYkomxBysjVimxtP7flrTee/iiueeZp5e/FRmu9NKyTpeLpS7O43a',
         }], {});
