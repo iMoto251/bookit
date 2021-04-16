d1a4191309e526dcb743540980cc549f4c86bdf9
wlkeith0 <whitneylynnkeith@gmail.com>
2021-04-16 11:35:27 -0500

    Initial sequelize models for book db added, recreated net2 config.

---
+++ packages/server/db/migrations/book.ts
@@ -0,0 +1,34 @@
+import sequelize from "sequelize";
+import Sequelize, { QueryInterface } from "sequelize";
+
+module.exports = {
+  async up({ context: queryInterface }: { context: QueryInterface }) {
+    await queryInterface.createTable("book", {
+      book-id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        // unique: true,
+      },
+      genre: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      isbn: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    //   createdOn: {
+    //     type: sequelize.DATE,
+    //     allowNull: false,
+    //   },
+    });
+  },
+  async down({ context: queryInterface }: { context: QueryInterface }) {
+    await queryInterface.dropTable("book");
+  },
+};
